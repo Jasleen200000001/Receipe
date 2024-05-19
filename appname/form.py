4180c240e31f73f8e0cab79ee5a2efc0a6dae1f7
Jasleen200000001 <122430570+Jasleen200000001@users.noreply.github.com>
2024-05-19 23:23:25 +0530

    delet for re upload
    delet for re upload

--- appname/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class Pythonform(forms.Form):
-    name = forms.CharField(min_length=2,max_length=100, required=True)
-    email = forms.EmailField()
-    age = forms.ImageField()
